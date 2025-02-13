96b808171fc7f481c68fb40ca785fa0246d27161
Karol Jurzec <karol.jurzec.programista@gmail.com>
2025-02-13 12:35:38 +0100

    Dodanie funkcji wczytujacej adresata do vectora

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <vector>
+#include <fstream>
 
 using namespace std;
+
+struct Adresat {
+    int idAdresata, idPrzypisanegoUzytkownika;
+    string imie, nazwisko, numerTelefonu, adres, email;
+
+    Adresat(int idAdresata = 0, int idUzytkownika = 0, string imie = "", string nazwisko = "", string telefon = "", string adres = "", string email = "")
+        : idAdresata(idAdresata), idPrzypisanegoUzytkownika(idUzytkownika), imie(imie), nazwisko(nazwisko), numerTelefonu(telefon), adres(adres), email(email) {}
+};
 
 string wczytajTekst() {
     string tekst = "";
@@ -46,6 +56,22 @@ int wczytajLiczbe() {
     }
 }
 
+string wczytajTekstDozwoloneZnaki(const string &komentarz) {
+    string tekst = "";
+
+    while(true) {
+        cout << komentarz;
+        tekst = wczytajTekst();
+
+        if(tekst.find("|") != string::npos)
+            cout << endl << "Nie mozna uzywac znaku '|'. Sprobuj ponownie!" << endl;
+        else if((tekst.front() == ' ') || (tekst.back() == ' '))
+            cout << endl << "Pierwszy oraz ostatni znak nie moga byc spacja" << endl;
+        else
+            return tekst;
+    }
+}
+
 void wstrzymajProgram() {
     cout << "Wcisnij Enter, aby kontynuowac...";
     cin.get();
@@ -68,7 +94,84 @@ void wyswietlMenuUzytkownika() {
     cout << "Wybierz jedna z opcji:\t";
 }
 
-void dodajAdresata()    {
+Adresat przypiszDaneAdresatowi(const string &odczytywanaLinia) {
+    int licznikPionowychKresek = 0;
+    Adresat wczytywanyAdresat;
+    string daneUzytkownika = "";
+
+    for(char znak : odczytywanaLinia) {
+        if(znak != '|') {
+            daneUzytkownika.push_back(znak);
+        } else {
+            licznikPionowychKresek++;
+            switch(licznikPionowychKresek) {
+                case 1:     wczytywanyAdresat.idAdresata = stoi(daneUzytkownika);                   break;
+                case 2:     wczytywanyAdresat.idPrzypisanegoUzytkownika = stoi(daneUzytkownika);    break;
+                case 3:     wczytywanyAdresat.imie = daneUzytkownika;                               break;
+                case 4:     wczytywanyAdresat.nazwisko = daneUzytkownika;                           break;
+                case 5:     wczytywanyAdresat.numerTelefonu = daneUzytkownika;                      break;
+                case 6:     wczytywanyAdresat.adres = daneUzytkownika;                              break;
+                case 7:     wczytywanyAdresat.email = daneUzytkownika;                              break;
+                default:
+                    cout << "Blad wczytywania. Dane Adresata nie moga zawierac znaku '|'. Ksiazka Adresowa zostanie zamknieta!" << endl;
+                    wstrzymajProgram();
+                    exit(1);
+            }
+            daneUzytkownika.clear();
+        }
+    }
+    return wczytywanyAdresat;
+}
+
+Adresat wczytajDaneNowegoAdresata(const int idZalogowanegoUzytkownika, const int idOstatniegoAdresata) {
+    system("cls");
+    Adresat nowyAdresat;
+
+    nowyAdresat.idAdresata = idOstatniegoAdresata + 1;
+    nowyAdresat.idPrzypisanegoUzytkownika = idZalogowanegoUzytkownika;
+
+    cout << "\t< DODAJ ADRESATA >" << endl;
+    cout << "----------------------------------" << endl << endl;
+    cout << "Numer ID adresata:\t\t" << nowyAdresat.idAdresata << endl;
+
+    nowyAdresat.imie = wczytajTekstDozwoloneZnaki("Podaj imie:\t\t\t");
+    nowyAdresat.nazwisko = wczytajTekstDozwoloneZnaki("Podaj nazwisko:\t\t\t");
+    nowyAdresat.numerTelefonu = wczytajTekstDozwoloneZnaki("Podaj numer telefonu:\t\t");
+    nowyAdresat.email = wczytajTekstDozwoloneZnaki("Podaj adres email:\t\t");
+    nowyAdresat.adres = wczytajTekstDozwoloneZnaki("Podaj adres zamieszkania:\t\t");
+
+    return nowyAdresat;
+}
+
+int pobierzIdOstatniegoAdresata() {
+    int idOstatniegoAdresata = 0;
+    string wczytanaLinia = "", ostatniaLinia = "";
+    fstream plikListaAdresatow;
+    Adresat wczytanyAdresat;
+
+    plikListaAdresatow.open("Adresaci.txt", ios::in);
+
+    if (plikListaAdresatow.good()) {
+        while (getline(plikListaAdresatow, wczytanaLinia)) {
+            if(!wczytanaLinia.empty())
+                ostatniaLinia = wczytanaLinia;
+        }
+        wczytanyAdresat = przypiszDaneAdresatowi(ostatniaLinia);
+        idOstatniegoAdresata = wczytanyAdresat.idAdresata;
+    }
+
+    plikListaAdresatow.close();
+
+    return idOstatniegoAdresata;
+}
+
+void dodajAdresata(vector <Adresat> &adresaci, const int idZalogowanegoUzytkownika) {
+    int idOstatniegoAdresata = 0;
+    Adresat nowyAdresat;
+
+    idOstatniegoAdresata = pobierzIdOstatniegoAdresata();
+    nowyAdresat = wczytajDaneNowegoAdresata(idZalogowanegoUzytkownika, idOstatniegoAdresata);
+
 }
 
 void wyszukajAdresataPoImieniu()    {
@@ -93,13 +196,18 @@ void wyloguj()    {
 }
 
 int main() {
+    vector <Adresat> adresaci;
+
+cout << "Podaj ID: ";
+int idZalogowanegoUzytkownika = 1;                      // podaj ID jakie bedzie docelowo podawal program
+
     while(true)    {
         wyswietlMenuUzytkownika();
         char wyborUzytkownika = wczytajLiczbe();
 
         switch (wyborUzytkownika) {
             case 1:
-                dodajAdresata();
+                dodajAdresata(adresaci, idZalogowanegoUzytkownika);
                 break;
             case 2:
                 wyszukajAdresataPoImieniu();
